8e6fa05f6d765b65310dc538cc5d818d429a9dda
dns-lector <157026463+dns-lector@users.noreply.github.com>
2025-10-15 11:51:36 +0300

    Groups

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,8 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import AppContext from "../../features/appContext/AppContext";
 
 export default function Admin() {
     const {token} = useContext(AppContext);
+    const [groups, setGroups] = useState([]);
+
+    const loadGroups = () => {
+        fetch("http://localhost:8080/JavaWeb222/admin/groups", {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        }).then(r => r.json()).then(setGroups);
+    };
+
+    useEffect(() => {
+        if(token) {
+            loadGroups();
+        }
+        else {
+            setGroups([]);
+        }
+    }, [token]);
+
+    const onProductFormSubmit = e => {
+        e.preventDefault();
+
+    }
 
     const onGroupFormSubmit = e => {
         e.preventDefault();
@@ -15,7 +39,16 @@ export default function Admin() {
         }).then(r => {
             let ct = r.headers.get("Content-Type");
             if(ct.startsWith("application/json")) {
-                r.json().then(console.log);
+                r.json().then(j => {
+                    if(j == "Ok") {
+                        e.target.reset();
+                        loadGroups();
+                        alert("Групу додано");
+                    }
+                    else {
+                        alert(j);
+                    }
+                });
             }
             else {
                 r.text().then(console.log);
@@ -77,10 +110,90 @@ export default function Admin() {
                     <div className="col">
                         <div className="input-group mb-3">
                             <label className="input-group-text" htmlFor="inputGroupSelect01">Підлеглість</label>
-                            <select className="form-select" id="inputGroupSelect01">
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
+                            <select name="pg-parent-id" className="form-select" id="inputGroupSelect01">
+                                <option value="">Без підлеглості</option>
+                                {groups.map(g => <option key={g.id} value={g.id}>{g.name}</option>)}
+                            </select>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <button type="submit" className="btn btn-primary">Додати</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div className="border m-3 p-2">
+            <h2>Додати товар</h2>
+            <form onSubmit={onProductFormSubmit} method="POST" encType="multipart/form-data">
+                <div className="row">
+                    <div className="col">
+                        <div className="input-group mb-3">
+                            <span className="input-group-text" id="product-name-addon">Назва</span>
+                            <input type="text" className="form-control" 
+                                name="product-name"
+                                placeholder="Назва" 
+                                aria-label="Назва" 
+                                aria-describedby="product-name-addon"/>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="input-group mb-3">
+                            <span className="input-group-text" id="product-description-addon">Опис</span>
+                            <input type="text" className="form-control" 
+                                name="product-description"
+                                placeholder="Опис" 
+                                aria-label="Опис" 
+                                aria-describedby="product-description-addon"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="input-group mb-3">
+                            <span className="input-group-text" id="product-price-addon">Ціна</span>
+                            <input type="number" min="0" step="0.01" className="form-control" 
+                                name="product-price"
+                                placeholder="Ціна" 
+                                aria-label="Ціна" 
+                                aria-describedby="product-price-addon"/>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="input-group mb-3">
+                            <span className="input-group-text" id="product-stock-addon">Кількість</span>
+                            <input type="number" min="0" step="1" className="form-control" 
+                                name="product-stock"
+                                placeholder="Кількість" 
+                                aria-label="Кількість" 
+                                aria-describedby="product-stock-addon"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="input-group mb-3">
+                            <span className="input-group-text" id="product-slug-addon">Slug</span>
+                            <input type="text" className="form-control" 
+                                name="product-slug"
+                                placeholder="Посилання на сторінку товару" 
+                                aria-label="Посилання на сторінку товару" 
+                                aria-describedby="product-slug-addon"/>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="input-group mb-3">
+                            <label className="input-group-text" htmlFor="product-image">Зображення</label>
+                            <input type="file" className="form-control" id="product-image" name="product-image"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="input-group mb-3">
+                            <label className="input-group-text" htmlFor="inputGroupSelect02">Група:</label>
+                            <select name="product-group-id" className="form-select" id="inputGroupSelect02">
+                                {groups.map(g => <option key={g.id} value={g.id}>{g.name}</option>)}
                             </select>
                         </div>
                     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,57 +1,35 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import AppContext from "../../features/appContext/AppContext";
 
 export default function Home() {
     const {token} = useContext(AppContext);
-    const [content, setContent] = useState("");
+    const [groups, setGroups] = useState([]);
 
-    const onPost = () => {
-        let headers = {
-            'Content-Type': "application/json"
-        };
-        if(token) {
-            headers["Authorization"] = "Bearer " + token;
-        }
-        fetch("http://localhost:8080/JavaWeb222/user", {
-            method: 'POST',
-            headers: headers
-        }).then(r => r.json()).then(j => setContent(JSON.stringify(j)));
+    const loadGroups = () => {
+        fetch("http://localhost:8080/JavaWeb222/groups")
+        .then(r => r.json()).then(setGroups);
     };
 
-    const invalid1 = () => {
-        fetch("http://localhost:8080/JavaWeb222/user", {
-            method: 'POST',
-            headers: {
-                "Authorization": "Brr 123"
-            }
-        }).then(r => r.json()).then(j => setContent(JSON.stringify(j)));
-    };
-    const invalid2 = () => {
-        fetch("http://localhost:8080/JavaWeb222/user", {
-            method: 'POST',
-            headers: {
-                "Authorization": "Bearer 1-2-3"
-            }
-        }).then(r => r.json()).then(j => setContent(JSON.stringify(j)));
-    };
-    const invalid3 = () => {
-        fetch("http://localhost:8080/JavaWeb222/user", {
-            method: 'POST',
-            headers: {
-                "Authorization": "Bearer 1.2.3"
-            }
-        }).then(r => r.json()).then(j => setContent(JSON.stringify(j)));
-    };
-
+    useEffect(() => {
+        loadGroups();
+    }, []);
 
     return <>
-        <h1>Home Page</h1>
-        <button onClick={onPost} type="button" className="btn btn-secondary">POST</button>
-        <button onClick={invalid1} type="button" className="btn btn-warning ms-1">Invalid scheme</button>
-        <button onClick={invalid2} type="button" className="btn btn-warning ms-1">Invalid structure</button>
-        <button onClick={invalid3} type="button" className="btn btn-warning ms-1">Invalid data</button>
-
-        {content && <div className="alert alert-primary" role="alert">{content}</div>}
+    <div style={{width:"100%", height:"25vh", overflow: "hidden", margin: "10px 0"}}>
+        <img src="/img/logo.jpg" className="w-100" />
+    </div>
+    
+    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4  row-cols-xxl-5 g-4">
+        {groups.map(g => <div className="col" key={g.id}>
+            <div className="card h-100">
+            <img src={g.imageUrl} className="card-img-top" alt={g.name} />
+            <div className="card-body">
+                <h5 className="card-title">{g.name}</h5>
+                <p className="card-text">{g.description}</p>
+            </div>
+            </div>
+        </div>)}
+    </div>        
     </>;
 }
 /*
